aefd02ecb607e5f75dd1cb93f15665f5611c024c
John Wass <wassj@ctc.com>
2018-10-28 21:20:34 -0400

    some additional args

--- hack/start-greyhound.sh
+++ hack/start-greyhound.sh
@@ -4,18 +4,19 @@ set -x
 
 # requires OpenShift source-to-image (s2i) be installed
 
-readonly builder=jwiii/greyhound-s2i
+readonly builder="${BUILDER:-jwiii/greyhound-s2i}"
 readonly server=example-pdal-greyhound
-readonly points=https://github.com/jw3/example-pointcloud-source.git
-readonly port="${1:-8080}"
+readonly points="${1:-https://github.com/jw3/example-pointcloud-source.git}"
+readonly port="${PORT:-8080}"
+readonly s2iopts="$S2I_OPTS"
 
 # check for greyhound-s2i built image
 docker images | grep -e "^$server" > /dev/null
 declare -i needs_built=${?}
 
 # if not present then build using entwine
-if [[ ${needs_built} -ne 0 ]]; then
-  s2i build "$points" "$builder" "$server"
+if [[ ! -z ${BUILDER} || ${needs_built} -ne 0 ]]; then
+  s2i build "$points" "$builder" "$server" "$s2iopts" -e http_proxy="$http_proxy" -e https_proxy="$https_proxy"
 fi
 
 # start greyhound
